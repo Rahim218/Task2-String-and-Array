1a5a9eb9d8413451d0c8c9d27ac3d92c1f073458
rahimvaliyev <rahimvaliyev99@gmail.com>
2022-12-27 19:28:55 +0400

    Arrays

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region Cut reqemlerin cemi
+//int toplam = 0;
+//for (int i = 0; i < 50; i++)
+
+//{
+//    if (i % 2 == 0)
+//    {
+//        toplam += i;
+//    }
+//}
+//Console.WriteLine(toplam);
+#endregion
+
+
+#region 5 ededin ortalamasini tapmaq
+//int[] numb = { 10, 22, 35, 45, 5 };
+//int toplam = 0;
+//for (int i = 0; i < numb.Length; i++)
+//{
+//    toplam += numb[i];
+//}
+//double avarage = toplam / numb.Length;
+//Console.WriteLine(avarage);
+#endregion
+
+
+
+#region 3 reqemden en boyuyu
+//int[] nums = { 10, 12, 15 };
+
+
+//    if (nums[0] > nums[1] && nums[0] > nums[2])
+//    {
+//    Console.WriteLine("En boyuk reqem budur : " + nums[0]);
+//    }
+//else if (nums[1] > nums[0] && nums[1] > nums[2])
+//{
+//    Console.WriteLine("En boyuk reqem budur : " + nums[1] );
+//}
+//else
+//{
+//    Console.WriteLine("En boyuk reqem budur : " + nums[2]);
+//}
+#endregion
+
+#region Hello world
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine("Hello World");
+//}
+#endregion
+
+
+#region Duzbucaqlinin sahesi ve perimetrinin hesablanmasi
+//Console.WriteLine("Duzbucaqlinin enini daxil edin :");
+//int eni = int.Parse(Console.ReadLine());
+//Console.WriteLine("Duzbucaqlinin uzunlugunu daxil edin :");
+//int uzunluq = int.Parse(Console.ReadLine());
+
+
+//if (eni > 0 && uzunluq> 0 && eni != uzunluq)
+//{
+//    int sahe = eni * uzunluq;
+//    int perimetr = 2 * (eni + uzunluq);
+//    Console.WriteLine("Duzbucaqlinin sahesi :" + " " + sahe);
+//    Console.WriteLine("Duzbucaqlinin perimetri :" + " " + perimetr);
+//}
+//else
+//{
+//    Console.WriteLine("Reqemleri yeniden daxil edin");
+//}
+#endregion
+
+
+
